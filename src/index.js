20ffe20a581c79fb87d44b9164e493d6a619e386
Yurimurda <lolsgotpwnd@yahoo.com>
2019-12-03 17:26:18 -0600

    My card made

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  state = {
+    user: {}
+  };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidMount() {
+    console.log("cDM");
+    // fetch("https://api.github.com/users/Yurimurda")
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     this.setState({ user: data.message });
+    //     console.log(data)
+    //   });
+      axios
+      .get("https://api.github.com/users/Yurimurda")
+      .then(response => {
+        console.log(response);
+        this.setState({ user: response.data });
+      })
+      .catch(err => console.log(err));
+  
+  }
+
+  componentDidUpdate() {
+    console.log("cDU");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Github user</h1>
+        <div className="user">
+          <img width="200" src={this.state.user.avatar_url}/>
+            <p className="name">Name: {this.state.user.name}</p>
+            <p className="bio">Bio: {this.state.user.bio}</p>
+            <p className="creationDate">Date of Creation: {this.state.user.created_at}</p>
+            <p className="followers">Followers: {this.state.user.followers}</p>
+            <p className="following">Following: {this.state.user.following}</p>
+        </div>
+      </div>
+    );
+  }
+}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
