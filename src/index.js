f5883162275a1083747f57759dc093108e76cde1
Yurimurda <lolsgotpwnd@yahoo.com>
2019-12-03 17:59:39 -0600

    My card made

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import "./index.css";
 
 class App extends React.Component {
   state = {
-    user: {}
+    user: {},
+    followers: []
   };
 
   componentDidMount() {
@@ -23,25 +24,36 @@ class App extends React.Component {
         this.setState({ user: response.data });
       })
       .catch(err => console.log(err));
-  
+      axios
+      .get(`https://api.github.com/users/Yurimurda/followers`)
+      .then(response => {
+        console.log(response);
+        this.setState({ followers: response.data.message });
+      })
+      .catch(err => console.log(err));
+
   }
 
   componentDidUpdate() {
     console.log("cDU");
   }
 
+
+
   render() {
     return (
       <div className="App">
         <h1>Github user</h1>
         <div className="user">
-          <img width="200" src={this.state.user.avatar_url}/>
+          <img width="200" src={this.state.user.avatar_url} alt={this.state.user.avatar_url}/>
             <p className="name">Name: {this.state.user.name}</p>
             <p className="bio">Bio: {this.state.user.bio}</p>
             <p className="creationDate">Date of Creation: {this.state.user.created_at}</p>
             <p className="followers">Followers: {this.state.user.followers}</p>
+            {/* <p className="followersList">{this.state.followers.map()}</p> */}
             <p className="following">Following: {this.state.user.following}</p>
         </div>
+        
       </div>
     );
   }
